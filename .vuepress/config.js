ddbe916398b53c2dc84096e9052c3195814df933
Dhinak G <dhinak2004@gmail.com>
2020-07-14 15:03:24 -0400

    fix stuffs

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -33,7 +33,7 @@ module.exports = {
         ["link", {
             rel: "'stylesheet",
             href: "/styles/website.css"
-        }, ]
+        },]
     ],
     base: '/OpenCore-Post-Install/',
 
@@ -58,85 +58,85 @@ module.exports = {
         nav: [{
             text: 'Dortania Guides',
             items: [{
-                    text: 'Home Site',
-                    link: 'https://dortania.github.io/'
-                },
-                {
-                    text: 'OpenCore Install Guide',
-                    link: 'https://dortania.github.io/OpenCore-Desktop-Guide/'
-                },
-                {
-                    text: 'GPU Buyers Guide',
-                    link: 'https://dortania.github.io/GPU-Buyers-Guide/'
-                },
-                {
-                    text: 'Wireless Buyers Guide',
-                    link: 'https://dortania.github.io/Wireless-Buyers-Guide/'
-                },
-                {
-                    text: 'Anti Buyers Guide',
-                    link: 'https://dortania.github.io/Anti-Hackintosh-Buyers-Guide/'
-                },
+                text: 'Home Site',
+                link: 'https://dortania.github.io/'
+            },
+            {
+                text: 'OpenCore Install Guide',
+                link: 'https://dortania.github.io/OpenCore-Desktop-Guide/'
+            },
+            {
+                text: 'GPU Buyers Guide',
+                link: 'https://dortania.github.io/GPU-Buyers-Guide/'
+            },
+            {
+                text: 'Wireless Buyers Guide',
+                link: 'https://dortania.github.io/Wireless-Buyers-Guide/'
+            },
+            {
+                text: 'Anti Buyers Guide',
+                link: 'https://dortania.github.io/Anti-Hackintosh-Buyers-Guide/'
+            },
             ]
-        }, ],
+        },],
         sidebar: [{
-                title: 'Introduction',
-                collapsable: false,
-                sidebarDepth: 1,
-                children: [
-                    '',
-                ]
+            title: 'Introduction',
+            collapsable: false,
+            sidebarDepth: 1,
+            children: [
+                '',
+            ]
 
-            },
-            {
-                title: 'Universal',
-                collapsable: false,
-                sidebarDepth: 1,
-                children: [
-                    ['/universal/security', 'Security and FileVault'],
-                    ['/universal/audio', 'Fixing Audio'],
-                    ['/universal/oc2hdd', 'Booting without USB'],
-                    ['/universal/update', 'Updating OpenCore, kexts and macOS'],
-                    ['/universal/drm', 'Fixing DRM'],
-                    ['/universal/iservices', 'Fixing iServices'],
-                    ['/universal/pm', 'Fixing Power Management'],
-                    ['/universal/sleep', 'Fixing Sleep'],
-                    ['https://dortania.github.io/USB-Map-Guide/', 'Fixing USB'],
-                ]
-            },
-            {
-                title: 'Laptop Specifics',
-                collapsable: false,
-                children: [
-                    ['/laptop-specific/battery', 'Fixing Battery Read-outs'],
+        },
+        {
+            title: 'Universal',
+            collapsable: false,
+            sidebarDepth: 1,
+            children: [
+                ['/universal/security', 'Security and FileVault'],
+                ['/universal/audio', 'Fixing Audio'],
+                ['/universal/oc2hdd', 'Booting without USB'],
+                ['/universal/update', 'Updating OpenCore, kexts and macOS'],
+                ['/universal/drm', 'Fixing DRM'],
+                ['/universal/iservices', 'Fixing iServices'],
+                ['/universal/pm', 'Fixing Power Management'],
+                ['/universal/sleep', 'Fixing Sleep'],
+                ['https://dortania.github.io/USB-Map-Guide/', 'Fixing USB'],
+            ]
+        },
+        {
+            title: 'Laptop Specifics',
+            collapsable: false,
+            children: [
+                ['/laptop-specific/battery', 'Fixing Battery Read-outs'],
 
-                ]
-            },
-            {
-                title: 'Cosmetics',
-                collapsable: false,
-                children: [
-                    ['/cosmetic/verbose', 'Fixing Resolution and Verbose'],
-                    ['/cosmetic/gui', 'Add GUI and Boot-chime'],
-                ]
-            },
-            {
-                title: 'Multiboot',
-                collapsable: false,
-                children: [
-                    ['/multiboot/bootstrap', 'Setting up Bootstrap.efi'],
-                    ['/multiboot/bootcamp', 'Installing BootCamp'],
-                ]
-            },
-            {
-                title: 'Miscellaneous',
-                collapsable: false,
-                children: [
-                    ['/misc/rtc', 'Fixing RTC'],
-                    ['/misc/msr-lock', 'Fixing CFG Lock'],
-                    ['/misc/nvram', 'Emulated NVRAM'],
-                ]
-            },
+            ]
+        },
+        {
+            title: 'Cosmetics',
+            collapsable: false,
+            children: [
+                ['/cosmetic/verbose', 'Fixing Resolution and Verbose'],
+                ['/cosmetic/gui', 'Add GUI and Boot-chime'],
+            ]
+        },
+        {
+            title: 'Multiboot',
+            collapsable: false,
+            children: [
+                ['/multiboot/bootstrap', 'Setting up Bootstrap.efi'],
+                ['/multiboot/bootcamp', 'Installing BootCamp'],
+            ]
+        },
+        {
+            title: 'Miscellaneous',
+            collapsable: false,
+            children: [
+                ['/misc/rtc', 'Fixing RTC'],
+                ['/misc/msr-lock', 'Fixing CFG Lock'],
+                ['/misc/nvram', 'Emulated NVRAM'],
+            ]
+        },
 
         ],
     },
@@ -145,8 +145,11 @@ module.exports = {
      * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
      */
     plugins: [
-        'vuepress-plugin-back-to-top',
+        '@vuepress/plugin-back-to-top',
         'vuepress-plugin-smooth-scroll',
-        '@vuepress/medium-zoom',
+        ['vuepress-plugin-medium-zoom',
+            {
+                "selector": "img"
+            }],
     ]
 }
